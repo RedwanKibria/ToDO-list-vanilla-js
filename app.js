154ce0e472de09307a17cd038a322a8b85f101fc
RedwanKibria <86778079+RedwanKibria@users.noreply.github.com>
2023-03-31 01:11:56 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
+
+//Event Listeners 
+document.addEventListener('DOMContentLoaded', getTodos);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
